4db75b0b29fbc622796d0b2a02c95a7e224b1fd6
Kenny Baron <kennybaron@fastmail.com>
2023-05-28 14:04:28 -0400

    fin day3

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,91 @@ use std::{
 };
 
 pub fn run() {
-    if let Err(e) = day3::run() {
+    if let Err(e) = day3::run_part2() {
         eprintln!("Application Error: {e}");
         std::process::exit(1);
     }
 }
 
 // https://adventofcode.com/2022/day/3
+// part1
+// fail for answer 10439 using non_unique misplaced, too high
+// 948 too low using unique misplaced
+// 7793 identifying only unique misplaced items per rucksack
+// part2 = 2499
 pub mod day3 {
-    use std::error::Error;
+    use std::{collections::HashSet, error::Error, fs};
 
-    pub fn run() -> Result<(), Box<dyn Error>> {
-        Ok(())
+    pub fn run_part1() -> Result<u32, Box<dyn Error>> {
+        let contents = fs::read_to_string("/home/kenny/code-exercises/aoc/rust-aoc/data/3_input")?;
+
+        // if fails solution, use HashSet
+        let mut all_misplaced = Vec::new();
+        // let mut misplaced_items = HashSet::new();
+
+        // parse each line into a map where: letter => key, (0,0) values, 1 if found for corresponding compartment
+        for line in contents.lines() {
+            let mut rucksack_misplaced = HashSet::new(); // misplaced items are uniquely identified per rucksack
+            let mut compart = HashSet::new();
+            let idx_split = line.len() / 2 - 1;
+
+            for c in line.chars().take(idx_split + 1) {
+                compart.insert(c);
+            }
+
+            for c in line.chars().skip(idx_split + 1) {
+                if compart.contains(&c) {
+                    // misplaced_items.push(c);
+                    rucksack_misplaced.insert(c);
+                }
+            }
+            all_misplaced.extend(rucksack_misplaced);
+            // let mut first_half = line.chars().take(idx_split + 1).collect::<Vec<char>>();
+            // println!("first half: {:?}", first_half);
+            // let mut second_half = line.chars().skip(idx_split + 1).collect::<Vec<char>>();
+            // println!("second half: {:?}", second_half);
+            // first_half.sort();
+            // second_half.sort();
+            // for i in 0..first_half.len() {
+            //     println!("{} {}", first_half[i], second_half[i]);
+            // }
+        }
+        println!("{:?}", all_misplaced);
+        let sum_priorities_nonunique = all_misplaced
+            .iter()
+            .fold(0, |acc, curr| acc + priority(curr));
+        println!("{sum_priorities_nonunique}");
+        Ok(sum_priorities_nonunique)
+    }
+
+    pub fn run_part2() -> Result<u32, Box<dyn Error>> {
+        let contents = fs::read_to_string("/home/kenny/code-exercises/aoc/rust-aoc/data/3_input")?;
+
+        let mut i = 1;
+        let mut sets: Vec<HashSet<char>> = Vec::new();
+        let mut sum: u32 = 0;
+        for line in contents.lines() {
+            sets.push(line.chars().collect::<HashSet<char>>());
+            if i == 3 {
+                for c in sets[0].iter() {
+                    if sets[1].contains(c) && sets[2].contains(c) {
+                        sum += priority(c);
+                    }
+                }
+                sets.clear();
+                i = 0;
+            }
+            i += 1;
+        }
+        println!("{sum}");
+        Ok(sum)
+    }
+
+    fn priority(c: &char) -> u32 {
+        if &c.to_lowercase().next().unwrap() == c {
+            return *c as u32 - 96;
+        }
+        return *c as u32 - 38;
     }
 }
 // part1: 13924
